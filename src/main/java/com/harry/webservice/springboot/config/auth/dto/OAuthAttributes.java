118de7cea1e748c267471af89f3665130855659c
harrykim-nsuslab <harry.kim@nsuslab.com>
2023-12-03 20:42:34 +0900

    Feat: OAuth2를 위한 Entity 및 DTO 생성

---
+++ src/main/java/com/harry/webservice/springboot/config/auth/dto/OAuthAttributes.java
@@ -0,0 +1,55 @@
+package com.harry.webservice.springboot.config.auth.dto;
+
+import com.harry.webservice.springboot.domain.user.Role;
+import com.harry.webservice.springboot.domain.user.User;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
+public class OAuthAttributes {
+
+    private final Map<String, Object> attributes;
+
+    private final String nameAttributeKey;
+
+    private final String name;
+
+    private final String email;
+
+    private final String picture;
+
+    @Builder
+    public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String name, String email, String picture) {
+        this.attributes = attributes;
+        this.nameAttributeKey = nameAttributeKey;
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+    }
+
+    public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
+        return OAuthAttributes.builder()
+                .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .name((String) attributes.get("name"))
+                .email((String) attributes.get("email"))
+                .picture((String) attributes.get("picture"))
+                .build();
+    }
+
+    public User toEntity() {
+        return User.builder()
+                .name(name)
+                .email(email)
+                .picture(picture)
+                .role(Role.GUEST)
+                .build();
+    }
+
+}
