9fb20d71f8c3081abe0e2bbc2804976f3216d4e7
harrykim-nsuslab <harry.kim@nsuslab.com>
2023-12-03 20:43:46 +0900

    Feat: Configuration 설정 (Audit, Security)

--- src/main/java/com/harry/webservice/springboot/config/audit/JpaAuditingConfig.java
+++ src/main/java/com/harry/webservice/springboot/config/audit/JpaAuditingConfig.java
@@ -1,9 +1,9 @@
-package com.harry.webservice.springboot.web.config;
+package com.harry.webservice.springboot.config.audit;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
 @EnableJpaAuditing
-public class JpaAuditingConfiguration {
+public class JpaAuditingConfig {
 }
